e736e3faf89a1231eb5b7c552f5b254a0e999fa3
Ryan Clark <clar1513@umn.edu>
2018-11-12 17:42:49 -0600

    Trimming MainAcrtivity

--- app/src/main/java/edu/umn/where_to_eat_app/MainActivity.java
+++ app/src/main/java/edu/umn/where_to_eat_app/MainActivity.java
@@ -1,11 +1,7 @@
 package edu.umn.where_to_eat_app;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,7 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,31 +20,40 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
             this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        fab.hide();
+        new Users();
 
-        // V COMMENT OUT THIS CODE TO SKIP LOGIN V
-        //startActivity(new Intent(MainActivity.this, Login.class));
+        // V FLAG TO SKIP LOGIN V
+        boolean skipLogin = false;
+
+        if(skipLogin) {
+            Users.setCurrentUser("Ryan");
+            TextView welcome = findViewById(R.id.welcomeText);
+            welcome.setText("Welcome, " + Users.getCurrentUser() + "!");
+        } else {
+            startActivityForResult(new Intent(MainActivity.this, Login.class), 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
+        if(requestCode == 1) {
+            if(resultCode == 1) {
+                TextView welcome = findViewById(R.id.welcomeText);
+                welcome.setText("Welcome, " + Users.getCurrentUser() + "!");
+            }
+        }
     }
 
     @Override
@@ -86,34 +91,15 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        Fragment fragment = null;
-        Bundle bundle = new Bundle();
+        Fragment fragment = MenuHandler.getFragment(item);
 
-        if (id == R.id.home) {
-            // Handle the home action
-            fragment = new JoinARoom();
-        } else if (id == R.id.profile) {
-            fragment = new ProfilePage();
-        } else if (id == R.id.create_account) {
-
-        } else if (id == R.id.notification) {
-
-        } else if (id == R.id.settings) {
-
-        } else if (id == R.id.rate_us) {
-
-        }
-
-        // replacing the fragment
         if(fragment != null) {
             android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.content_frame, fragment);
             ft.commit();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
